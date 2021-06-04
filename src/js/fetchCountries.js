f7996d1a86493173494e98a26375de2723e83a10
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-06-04 13:07:57 +0200

    adds async await method

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,27 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 
-function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => {
-    if (response.ok === false) {
-      return error({ delay: 1000, text: 'Nothing found' }); // in case fetch reacts wrong on error 404
-    }
+// classic method
 
-    return response.json();
-  });
+// function fetchCountries(searchQuery) {
+//   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => {
+//     if (response.ok === false) {
+//       return error({ delay: 1000, text: 'Nothing found' }); // in case fetch reacts wrong on error 404
+//     }
+
+//     return response.json();
+//   });
+// }
+
+// async await method
+
+async function fetchCountries(searchQuery) {
+  const response = await fetch(`${BASE_URL}/name/${searchQuery}`);
+  const countries = response.json();
+
+  if (response.ok === false) {
+    return error({ delay: 1000, text: 'Nothing found' }); // in case fetch reacts wrong on error 404
+  }
+  return countries;
 }
 
 export default { fetchCountries };
